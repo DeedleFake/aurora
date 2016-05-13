cb349c823f13072f5879422209a0845fb62f0e3a
DeedleFake <deedlefake@users.noreply.github.com>
2016-05-13 00:10:27 -0400

    cmd/aurora: Hook up pacman package to pacmanMode.

--- cmd/aurora/pacman.go
+++ cmd/aurora/pacman.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"github.com/DeedleFake/aurora/flag"
+	"github.com/DeedleFake/aurora/pacman"
+	"os"
+	"os/exec"
+	"syscall"
+)
+
 type pacmanMode struct{}
 
 var (
@@ -7,5 +15,11 @@ var (
 )
 
 func (m pacmanMode) Main() {
-	panic("Not implemented.")
+	err := pacman.Run(flag.Args...)
+	if err != nil {
+		switch err := err.(type) {
+		case *exec.ExitError:
+			os.Exit(err.Sys().(syscall.WaitStatus).ExitStatus())
+		}
+	}
 }
